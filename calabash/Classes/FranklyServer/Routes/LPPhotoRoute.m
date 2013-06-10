fea5a35f399b1c8be9d5406f9680d4f93f681c07
iplukens <iplukens@wpi.edu>
2013-06-10 13:02:57 -0400

    Fixed the return and added more options within the PhotoRoute for just adding album/photo/both

--- calabash/Classes/FranklyServer/Routes/LPPhotoRoute.m
+++ calabash/Classes/FranklyServer/Routes/LPPhotoRoute.m
@@ -20,45 +20,51 @@
 - (NSDictionary *)JSONResponseForMethod:(NSString *)method URI:(NSString *)path data:(NSDictionary*)data {
     // Receive the photo data
     NSMutableString *encodedData = [data objectForKey:@"phto"];
+    if(encodedData != nil){
+        // Decode the base64 encoded data
+        [Base64 initialize];
+        NSData * imageData = [Base64 decode:encodedData];
     
-    // Decode the base64 encoded data
-    [Base64 initialize];
-    NSData * imageData = [Base64 decode:encodedData];
+        // create image
+        UIImage *image = [[UIImage alloc]initWithData:imageData];
     
-    // create image
-    UIImage *image = [[UIImage alloc]initWithData:imageData];
-    
-    if(image == nil){
-        // add code for failure to decode picture data
-        return [NSDictionary dictionaryWithObjectsAndKeys:
+        if(image == nil){
+            // add code for failure to decode picture data
+            return [NSDictionary dictionaryWithObjectsAndKeys:
                 @"photo not added", @"results",
                 @"FAILURE",@"outcome",
                 @"Decode failed", @"reason",
                 @"wrong size for decode; base64 string must be divisible by 4", @"details", nil];
+        }
     }
-    
-    // KEXIN'S CODE HERE //
     NSString *album = [data objectForKey:@"album"];
     if(album != nil){
-        // add to specific album
-        
+        // create album
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        [library addAssetsGroupAlbumWithName:album
+                                 resultBlock:^(ALAssetsGroup *group) {
+                                     NSLog(@"added album:%@", album);
+                                 }
+         
+                                failureBlock:^(NSError *error) {
+                                    NSLog(@"error adding album");
+                                }
+         ];
+        if(encodedData != nil){
+         // TODO: add to specific album
+        }
+
     }
     else{
-        // add to default album
+        if(encodedData != nil){
+        // TODO: add to default album
+        }
     }
     
-    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-    
-    [library addAssetsGroupAlbumWithName:album
-                             resultBlock:^(ALAssetsGroup *group) {
-                                 NSLog(@"added album:%@", album);
-                             }
-     
-                            failureBlock:^(NSError *error) {
-                                NSLog(@"error adding album");
-                            }
-     ];
-
+    return [NSDictionary dictionaryWithObjectsAndKeys:
+            @"photo added", @"results",
+            @"SUCCESS",@"outcome",
+            nil];
 }
 
 @end

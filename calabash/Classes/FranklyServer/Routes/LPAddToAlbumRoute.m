ae37727ddc12dad94191539a364ca5938ecb42e5
iplukens <iplukens@wpi.edu>
2013-06-13 14:33:46 -0400

    background methods still failed but looks nicer! (only one return call)

--- calabash/Classes/FranklyServer/Routes/LPAddToAlbumRoute.m
+++ calabash/Classes/FranklyServer/Routes/LPAddToAlbumRoute.m
@@ -16,6 +16,9 @@
 @synthesize library;
 
 BOOL complete = false;
+NSString *errorMsg;
+BOOL flag = false;
+NSString *result;
 
 
 -(BOOL)supportsMethod:(NSString *)method atPath:(NSString *)path {
@@ -24,13 +27,13 @@ BOOL complete = false;
 
 // Decode the base64 encoded data
 - (NSData*) decodeFile:(NSString*)data{
-    NSData * result = [LPResources decodeBase64WithString:data];
-    return result;
+    NSData * file = [LPResources decodeBase64WithString:data];
+    return file;
 }
 
 
 // adds the video to the saved photos album
-- (NSDictionary *) videoFunction:(NSData*)videoData {    
+- (void) videoFunction:(NSData*)videoData {
     // get path to save video locally to ios system
     NSArray   *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString  *documentsDirectory = [paths objectAtIndex:0];
@@ -43,27 +46,22 @@ BOOL complete = false;
     if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(filePath)){
         // move data to the album
         UISaveVideoAtPathToSavedPhotosAlbum(filePath, self, @selector(video:didFinishSavingWithError:contextInfo:), filePath);
-        // return
-        return [NSDictionary dictionaryWithObjectsAndKeys:
-                @"video added", @"results",
-                @"SUCCESS",@"outcome",
-                nil];
+        // set return values
+        result = @"video added";
     }
     // return error - incorrect format
     else {
-        return [NSDictionary dictionaryWithObjectsAndKeys:
-                @"failed to write to album", @"results",
-                @"FAILURE",@"outcome",
-                @"file at path not compatible with m4v format", @"reason",
-                nil];
+        flag = true;
+        complete = true;
+        result = @"file at path not compatible with m4v format";
     }
 }
 
 - (void)video:(NSString *)videoPath didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
     complete = true;
     if(error != nil){
-        NSLog(@"error with saving video");
-        NSLog(@"details: %@", [error description]);
+        flag = true;
+        errorMsg = [error description];
     }
     else{
         // delete the now duplicated data
@@ -77,76 +75,77 @@ BOOL complete = false;
 }
 
 // adds the photo to an album
-- (NSDictionary *) photoFunction:(NSData*)imageData toAlbum:(NSString*)album {
+- (void) photoFunction:(NSData*)imageData toAlbum:(NSString*)album {
     
     UIImage *image = [[UIImage alloc]initWithData:imageData];
     
     // check if decoding went as expected; if not, return error
     if(image == nil){
-        return [NSDictionary dictionaryWithObjectsAndKeys:
-                @"photo not added", @"results",
-                @"FAILURE",@"outcome",
-                @"Decode failed", @"reason",
-                @"wrong size for decode; base64 string must be divisible by 4", @"details", nil];
+        flag = true;
+        result = @"Decode failed; file type incorrect";
     }
-    
-    // check if query requests specific album
-    if(![album isEqualToString:@"default"]){
-        self.library = [[ALAssetsLibrary alloc] init];
-        __block BOOL flag = false;
-        __block NSString *errorMsg;
-        // add it to the album using a background method
-        void (^completionBlock)(NSError*) = ^(NSError *error){
-            complete = true;
-            if (error!=nil) {
-                errorMsg = [error description];
-                flag = true;
-            }
-        };
-        
-    /* BACKGROUND METHOD
-    // NOT CURRENTLY WORKING - HANGS
-    // THIS IS NEEDED ON BOTH THE PHOTO AND VIDEO FUNCTIONS FOR PROPER ERROR REPORTING
-    // CURRENTLY FAILURES of saveImage:toAlbum:etc., UISaveVideoAtPathetc. WILL GO UNREPORTED
-         
-        NSMethodSignature *sig = [library methodSignatureForSelector:@selector(saveImage:toAlbum:withCompletionBlock:)];
-        NSInvocation *invoke = [NSInvocation invocationWithMethodSignature:sig];
-        [invoke setTarget:library];
-        [invoke setSelector:@selector(saveImage:toAlbum:withCompletionBlock:)];
-        [invoke setArgument:&image atIndex:2];
-        [invoke setArgument:&album atIndex:3];
-        [invoke setArgument:&completionBlock atIndex:4];
-        [invoke performSelectorInBackground:@selector(invoke) withObject:nil];
-        
-        while(!complete){
+    else{
+        // check if query requests specific album
+        if(![album isEqualToString:@"default"]){
+            self.library = [[ALAssetsLibrary alloc] init];
+            __block BOOL flag = false;
+            __block NSString *errorMsg;
+            // add it to the album using a background method
+            void (^completionBlock)(NSError*) = ^(NSError *error){
+                complete = true;
+                if (error!=nil) {
+                    errorMsg = [error description];
+                    flag = true;
+                }
+            };
             
-        }
-    */
-        
-        [library saveImage:image toAlbum:album withCompletionBlock:completionBlock];
-        // check for error
-        if (flag){
-            return [NSDictionary dictionaryWithObjectsAndKeys:
-                    @"album not added", @"results",
-                    @"FAILURE",@"outcome",
-                    @"Album addition failed", @"reason",
-                    errorMsg, @"details", nil];
+        /*
+        // BACKGROUND METHOD
+        // NOT CURRENTLY WORKING - HANGS
+        // THIS IS NEEDED ON BOTH THE PHOTO AND VIDEO FUNCTIONS FOR PROPER ERROR REPORTING
+        // CURRENTLY FAILURES of saveImage:toAlbum:etc., UISaveVideoAtPathetc. WILL GO UNREPORTED
+             
+             NSMethodSignature *sig = [library methodSignatureForSelector:@selector(saveImage:toAlbum:withCompletionBlock:)];
+             NSInvocation *invoke = [NSInvocation invocationWithMethodSignature:sig];
+             [invoke setTarget:library];
+             [invoke setSelector:@selector(saveImage:toAlbum:withCompletionBlock:)];
+             [invoke setArgument:&image atIndex:2];
+             [invoke setArgument:&album atIndex:3];
+             [invoke setArgument:&completionBlock atIndex:4];
+             [invoke performSelectorInBackground:@selector(invoke) withObject:nil];
+        */
             
+            [library saveImage:image toAlbum:album withCompletionBlock:completionBlock];
+            // check for error
         }
+        else {
+            // save to default photos if no album provided
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+        }
+        result = @"photo added";
     }
-    else {
-        // save to default photos if no album provided
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
-    }
-    return [NSDictionary dictionaryWithObjectsAndKeys:
-            @"photo added", @"results",
-            @"SUCCESS",@"outcome",
-            nil];
+}
+
+- (void) albumFunction:(NSString *)album{
+    [library addAssetsGroupAlbumWithName:album
+                             resultBlock:^(ALAssetsGroup *group) {
+                                 result = @"Album added";
+                                 complete = true;
+                             }
+                            failureBlock:^(NSError *error) {
+                                result = @"Album not added";
+                                errorMsg = [error description];
+                                complete = true;
+                                flag = true;
+                            }
+     ];
+    result = @"Album added";
 }
 
 
 // "main" method for returning a response to calabash client
 - (NSDictionary *)JSONResponseForMethod:(NSString *)method URI:(NSString *)path data:(NSDictionary*)data {
+    flag = false;
     // Receive the media data
     NSString *encodedData = [data objectForKey:@"media"];
     NSString *type = [data objectForKey:@"type"];
@@ -154,44 +153,42 @@ BOOL complete = false;
     // decode it
     NSData *binaryData = [self decodeFile:encodedData];
     
-    __block NSString *errorMsg;
-    __block BOOL flag = false;
-    
     // check if the file is a video
     if ([type isEqualToString:@"video"]){
-        // return the video operation
-        return [self videoFunction:binaryData];
+        // execute the video operation
+        [self videoFunction:binaryData];
     }
+    
     // check if it is a photo
     else if (encodedData != nil) {
-        // return the photo operation
-        return [self photoFunction:binaryData toAlbum:album];
+        // execute the photo operation
+        [self photoFunction:binaryData toAlbum:album];
     }
+    
     // otherwise, just create an album
     else{
-        [library addAssetsGroupAlbumWithName:album
-                                 resultBlock:^(ALAssetsGroup *group) {
-                                 }
-                                failureBlock:^(NSError *error) {
-                                    errorMsg = [error description];
-                                    flag = true;
-                                }
-         ];
-        // check for error
-        if(flag){
-            return [NSDictionary dictionaryWithObjectsAndKeys:
-                    @"album not added", @"results",
-                    @"FAILURE",@"outcome",
-                    @"Album addition failed", @"reason",
-                    errorMsg, @"details", nil];
-            
-        }
-        // return success!
+        [self albumFunction:album];
+    }
+    
+    // wait for completion
+    // while(!complete){
+        // busily
+    // }
+    
+    // check for error
+    if(flag){
         return [NSDictionary dictionaryWithObjectsAndKeys:
-                @"album added", @"results",
+                @"FAILURE",@"outcome",
+                result, @"reason",
+                errorMsg, @"details", nil];
+        
+    }
+    // return success!
+    else{
+        return [NSDictionary dictionaryWithObjectsAndKeys:
+                result, @"results",
                 @"SUCCESS",@"outcome",
                 nil];
-        
     }
 }
 
